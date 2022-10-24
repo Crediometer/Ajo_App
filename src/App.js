adcd3e3c9056bda74f4b09f0fc11d080cf122b84
Precious1324 <63919279+Samwell10@users.noreply.github.com>
2022-10-24 15:36:53 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -36,7 +36,6 @@ function App() {
         <Route exact path='/Otp' element={<Otp/>}></Route>
         <Route exact path='/ResetPassword' element={<Reset/>}></Route>
         <Route exact path='/dashboard' element={<Dashboard/>}></Route>
-        {/* ROUTE FOR PROFILE */}
         <Route exact path='/profile' element={<Profile/>}></Route>
         {/* ROUTE FOR TRANSACTION */}
         <Route exact path='/transaction' element={<Transaction/>}></Route>
